d099619602cc60956c1a222985605fc5fc32378c
hhui64 <907322015@qq.com>
2021-02-17 06:19:11 +0800

    feat(index): export all module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-export {}
+import Chat from './lib/Chat/Chat'
+import ChatListener from './lib/Chat/ChatListener'
+import ChatMethods from './lib/Chat/ChatMethods'
+import * as ChatInterface from './lib/Chat/ChatInterface'
+import Client from './lib/Client/Client'
+import ClientMethods from './lib/Client/ClientMethods'
+import * as ClientInterface from './lib/Client/ClientInterface'
+import Gift from './lib/Gift/Gift'
+import GiftListener from './lib/Gift/GiftListener'
+import GiftMethods from './lib/Gift/GiftMethods'
+import * as GiftInterface from './lib/Gift/GiftInterface'
+import Room from './lib/Room/Room'
+import RoomListener from './lib/Room/RoomListener'
+import RoomMethods from './lib/Room/RoomMethods'
+import * as RoomInterface from './lib/Room/RoomInterface'
+import User from './lib/User/User'
+import UserMethods from './lib/User/UserMethods'
+import * as UserInterface from './lib/User/UserInterface'
+
+export {
+  Chat,
+  ChatListener,
+  ChatMethods,
+  ChatInterface,
+  Client,
+  ClientMethods,
+  ClientInterface,
+  Gift,
+  GiftListener,
+  GiftMethods,
+  GiftInterface,
+  Room,
+  RoomListener,
+  RoomMethods,
+  RoomInterface,
+  User,
+  UserMethods,
+  UserInterface,
+}
